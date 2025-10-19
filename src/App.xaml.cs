e76819aeb1671c44857436f3bd8b481b7b602cbb
Олександр Піроженко <xizalis@outlook.com>
2025-10-19 16:24:38 +0300

    Minor code cleanup

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,14 +1,8 @@
 ﻿using System.Configuration;
-using System.Data;
 using System.Windows;
 
 namespace PowerOff
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
-    {
-    }
-
+    { }
 }
