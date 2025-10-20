ecd1016206c1051eeb390082cd2313431a7e4acb
Олександр Піроженко <xizalis@outlook.com>
2025-10-20 22:14:10 +0300

    Add some mathematical functions for Point class

---
+++ src/Math/Avg.cs
@@ -0,0 +1,29 @@
+﻿using PowerOff.Primitives;
+using System;
+
+namespace PowerOff
+{
+    public static partial class Math
+    {
+        public static Point Avg(Point p1, Point p2)
+        {
+            double x = (p1.X + p2.X) / 2;
+            double y = (p1.Y + p2.Y) / 2;
+            return new Point(x, y);
+        }
+
+        public static Point Avg(params Point[] points)
+        {
+            double x = 0;
+            double y = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                x += points[i].X;
+                y += points[i].Y;
+            }
+            x /= points.Length;
+            y /= points.Length;
+            return new Point(x, y);
+        }
+    }
+}
---
+++ src/Math/Distance.cs
@@ -0,0 +1,14 @@
+﻿using PowerOff.Primitives;
+using System;
+namespace PowerOff
+{
+    public static partial class Math
+    {
+        public static double Distance(Point a, Point b)
+        {
+            double dx = a.X - b.X;
+            double dy = a.Y - b.Y;
+            return System.Math.Sqrt(dx*dx + dy*dy);
+        }
+    }
+}
