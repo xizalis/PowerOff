3b0b15dd54ce4646697c09b00ba83fee708dfdb7
Олександр Піроженко <xizalis@outlook.com>
2025-10-23 17:49:30 +0300

    Add A3 some logic

---
+++ src/Math/A3.cs
@@ -0,0 +1,26 @@
+﻿using PowerOff.Primitives;
+using System;
+
+namespace PowerOff
+{
+    // Format A3 - 297×420 мм
+
+    public class A3
+    {
+        double height = 420;
+        double width = 297;
+
+        public void InitABCD()
+        {
+            double h2 = height / 2;
+            double w2 = width / 2;
+
+            Point A = new Point(-w2, +h2);
+            Point B = new Point(+w2, +h2);
+            Point C = new Point(+w2, -h2);
+            Point D = new Point(-w2, -h2);
+
+            //TODO - Add A B C D to Scope
+        }
+    }
+}
