5eab7fd688f6815ebc41b01db48d50053f4d2583
Олександр Піроженко <xizalis@outlook.com>
2025-10-27 19:25:00 +0200

    Add IntersectionFinder as a service above the AnalyticGeometry logic

---
+++ src/Intersection/IIntersectionFinder.cs
@@ -0,0 +1,14 @@
+﻿using PowerOff.Primitives;
+using System;
+
+namespace PowerOff
+{
+    interface IIntersectionFinder
+    {
+        (Point, Point) Intersect(Line l, Circle c);
+
+        (Point, Point) Intersect(Circle c1, Circle c2);
+
+        Point Intersect(Line l1, Line l2);
+    }
+}
---
+++ src/Intersection/IntersectionFinder.cs
@@ -0,0 +1,17 @@
+﻿using PowerOff.Primitives;
+using System;
+
+namespace PowerOff
+{
+    public class IntersectionFinder : IIntersectionFinder
+    {
+        public (Point, Point) Intersect(Line l, Circle c)
+            => AnalyticGeometry.Intersect(l, c);
+
+        public (Point, Point) Intersect(Circle c1, Circle c2)
+            => AnalyticGeometry.Intersect(c1, c2);
+
+        public Point Intersect(Line l1, Line l2)
+            => AnalyticGeometry.Intersect(l1, l2);
+    }
+}
