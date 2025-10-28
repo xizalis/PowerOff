323b12856535cdb9099147b3533ac38d3d2e1806
Олександр Піроженко <xizalis@outlook.com>
2025-10-28 16:01:18 +0200

    Add Page primitive

---
+++ src/Primitives/Page.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PowerOff.Primitives
+{
+    public struct Page
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Page(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
