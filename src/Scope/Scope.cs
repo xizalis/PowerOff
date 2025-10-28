f56433e891ac14c5ac5e14588345104d16ae6b71
Олександр Піроженко <xizalis@outlook.com>
2025-10-28 15:43:27 +0200

    Add SymbolTable and Scope

---
+++ src/Scope/Scope.cs
@@ -0,0 +1,21 @@
+﻿using PowerOff.Primitives;
+using System;
+
+namespace PowerOff
+{
+    public class Scope
+    {
+        public Scope() //ctor for main scope
+        { Parrent = null; }
+        public Scope(Scope parent)
+        { Parrent = parent; }
+
+        public SymbolTable<Point> Points { get; } = new();
+        public SymbolTable<Line> Lines { get; } = new();
+        public SymbolTable<Circle> Circles { get; } = new();
+
+        //---
+
+        public Scope? Parrent { get; private set; }
+    }
+}
---
+++ src/Scope/SymbolTable.cs
@@ -0,0 +1,50 @@
+﻿using PowerOff.Primitives;
+using System;
+
+namespace PowerOff
+{
+    public class SymbolTable<T> where T : struct
+    {
+        Dictionary<string, T> items = new();
+
+        public void Set(string name, T value)
+        {
+            if (NotValid(name))
+                return;
+            if (items.ContainsKey(name))
+                items[name] = value;
+            else
+                items.Add(name, value);
+        }
+
+        public T Get(string name)
+        {
+            if (NotValid(name))
+                return default;
+            if (items.ContainsKey(name))
+                return items[name];
+            return default;
+        }
+
+        public void Remove(string name)
+        {
+            if (NotValid(name))
+                return;
+            if (items.ContainsKey(name))
+                items.Remove(name);
+        }
+
+        //---
+
+        public T this[string name]
+        {
+            get => Get(name);
+            set => Set(name, value);
+        }
+
+        //---
+
+        bool NotValid(string name)
+            => string.IsNullOrWhiteSpace(name);
+    }
+}
