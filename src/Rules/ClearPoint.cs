16fa92ae16b8afd844c956ac52327a380c9d8cfc
Олександр Піроженко <xizalis@outlook.com>
2025-11-07 12:16:54 +0200

    Add ClearPoint rule

---
+++ src/Rules/ClearPoint.cs
@@ -0,0 +1,33 @@
+﻿using PowerOff.Primitives;
+using System;
+using System.Text.RegularExpressions;
+
+namespace PowerOff
+{
+    internal class ClearPoint : AbstractRule
+    {
+        public override void Handle(string request)
+        {
+            string PointName = @"^[A-Za-z]{1,3}\'{0,3}$";
+            string pattern = $"({PointName})=_";
+
+            var match = Regex.Match(request, pattern);
+            if (match.Groups.Count == 0)
+            {
+                nextRule?.Handle(request);
+                return;
+            }
+
+            //---
+
+            string firstPointName = match.Groups[0].Value;
+
+            //---
+
+            Scope scope = new Scope();
+
+            if (scope.Points.Exist(firstPointName))
+                scope.Points.Remove(firstPointName);
+        }
+    }
+}
