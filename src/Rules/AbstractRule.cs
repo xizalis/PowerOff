6906551519e08cb1afba5c1eeb8c1eb533010663
Олександр Піроженко <xizalis@outlook.com>
2025-11-07 12:12:53 +0200

    Add IRule and AbstractRule

---
+++ src/Rules/AbstractRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PowerOff
+{
+    internal abstract class AbstractRule : IRule
+    { 
+        protected IRule? nextRule;
+
+        public void SetNext(IRule rule)
+            => nextRule = rule;
+
+        public abstract void Handle(string powerOffCode);
+    }
+}
---
+++ src/Rules/IRule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PowerOff
+{
+    interface IRule
+    {
+        void SetNext(IRule rule);
+        void Handle(string powerOffCode);
+    }
+}
