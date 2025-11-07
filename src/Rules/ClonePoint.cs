9bdd516132e4d9823336f714712a5a01d1581111
Олександр Піроженко <xizalis@outlook.com>
2025-11-07 12:14:46 +0200

    Add ClonePoint rule

---
+++ src/Rules/ClonePoint.cs
@@ -0,0 +1,35 @@
+﻿using PowerOff.Primitives;
+using System;
+using System.Text.RegularExpressions;
+
+namespace PowerOff
+{
+    internal class ClonePoint : AbstractRule
+    {
+        public override void Handle(string request)
+        {
+            string PointName = @"^[A-Za-z]{1,3}\'{0,3}$";
+            string pattern = $"({PointName})=({PointName})";
+
+            var match = Regex.Match(request, pattern);
+            if (match.Groups.Count == 0)
+            {
+                nextRule?.Handle(request);
+                return;
+            }
+
+            //---
+
+            string firstPointName = match.Groups[0].Value;
+            string secondPointName = match.Groups[1].Value;
+
+            Scope scope = new Scope();
+
+            if (!scope.Points.Exist(secondPointName))
+                return;
+
+            Point secondPoint = scope.Points.Get(secondPointName);
+            scope.Points.Set(firstPointName, secondPoint);
+        }
+    }
+}
